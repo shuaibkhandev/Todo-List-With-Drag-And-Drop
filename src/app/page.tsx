b5215dffa5ae5a18ae4916df6130a38409ad6bb0
shuaibkhan_unitedsol <shuaib.khan@unitedsol.net>
2026-02-26 13:56:33 +0500

    added update and delete func

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import AddTodo from "@/components/AddTodo";
 import TodoList from "@/components/TodoList";
+import { todo } from "node:test";
 import { useEffect, useState } from "react";
 
 export interface Todo {
@@ -33,13 +34,27 @@ const Home = () => {
 
     setTodos((prevTodos) => [newTodo, ...prevTodos]);
   };
+
+  const updateTodo = (id:string, newText:string): void => {
+   setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, text: newText } : todo
+      )
+    );
+  }
+
+  const deleteTodo = (id:string) => {
+   setTodos((prev)=> prev.filter((todo) => todo.id != id))
+    
+    
+  }
   return (
     <>
       <div className="todo-list-wrapper bg-gray-600 min-h-screen px-2 py-5 md:pt-20">
         <div className="today-list-container w-full md:w-8/12 p-2  sm:p-5 rounded m-auto bg-white">
           <h2 className="text-4xl text-center font-bold">Todo List</h2>
           <AddTodo onAdd={addTodo} />
-          <TodoList todos={todos} />
+          <TodoList todos={todos} onUpdate={updateTodo} onDelete={deleteTodo} />
         </div>
       </div>
     </>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,32 +1,79 @@
 import { Todo } from "@/app/page";
+import { useEffect, useRef, useState } from "react";
 import { FaTrash, FaEdit, FaArrowsAlt } from "react-icons/fa";
 
 interface TodoListProps {
-  todos: Todo[]
+  todos: Todo[];
+  onUpdate: (id:string, newText:string) => void;
+  onDelete: (id:string) => void;
 }
 
-const TodoList = ({todos}:TodoListProps) => {
+const TodoList = ({ todos, onUpdate, onDelete }: TodoListProps) => {
+  const [editingId, setEditingId] = useState<string|null>(null);
+  const [editText, setEditText] = useState<string>("")
+  const selectedInput = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+  if (selectedInput.current) {
+    selectedInput.current.focus();
+  }
+}, [editingId]);
+
+  const handleEdit = (id:string, currentText:string):void => {
+    setEditingId(id)
+    setEditText(currentText)
+  }
+
+  const handleSave = (id:string) => {
+    if (!editText.trim()) return;
+     onUpdate(id, editText);
+    setEditingId(null);
+  }
+
+  const handleDelet = (id:string) => {
+    onDelete(id)
+  }
+  
   return (
-    <div className='todo-list flex flex-col gap-1 justify-center mt-8'>
-        {todos.map((todo)=>{
+    <div className="todo-list flex flex-col gap-1 justify-center mt-8">
+      {todos?.map((todo) => {
+        return (
+          <div key={todo.id} className="flex">
+            <button className="bg-gray-200 px-3 rounded mr-2 cursor-grab text-2xl font-medium">
+              <FaArrowsAlt color="gray" />
+            </button>
+            {todo.id === editingId ?   <input
+            ref={selectedInput}
+                className="py-2 px-4  w-full text-2xl border-2 border-gray-200 rounded outline-0"
+                value={editText}
+                onChange={(e) => setEditText(e.target.value)}
+              /> :   <input
+                className="py-2 px-4  w-full text-2xl border-2 border-gray-200 rounded outline-0"
+                value={todo.text}
+                readOnly
+              /> }
+            
+            {todo.id === editingId ?     <button
+              className="bg-gray-200 px-3 rounded ml-2 cursor-pointer text-2xl font-medium"
+              onClick={() => handleSave(todo.id,)}
+            >
+              save
+            </button> :     <button
+              className="bg-gray-200 px-3 rounded ml-2 cursor-pointer text-2xl font-medium"
+              onClick={() => handleEdit(todo.id, todo.text)}
+            >
+              <FaEdit size={20} color="blue" />
+            </button>}
 
-return(
-    <div key={todo.id} className="flex">
-    <button className='bg-gray-200 px-3 rounded mr-2 cursor-grab text-2xl font-medium'><FaArrowsAlt color="gray" /></button>
-    <div className="relative w-full">
-      {/* <span className="absolute bg-green-400 w-6.5 h-6.5 rounded-2xl text-center -ml-1.25 text-white">{index+1}</span> */}
-    <input className='py-2 px-4  w-full text-2xl border-2 border-gray-200 rounded outline-0' value={todo.text} readOnly/>
+        
+            <button className="bg-gray-200 px-3 rounded ml-2 cursor-pointer text-2xl font-medium" onClick={()=>handleDelet(todo.id)}>
+              <FaTrash size={20} color="red" />
+            </button>
+          </div>
+        );
+      })}
     </div>
-    <button className='bg-gray-200 px-3 rounded ml-2 cursor-pointer text-2xl font-medium'><FaEdit size={20} color="blue" /></button>
-        <button className='bg-gray-200 px-3 rounded ml-2 cursor-pointer text-2xl font-medium'><FaTrash size={20} color="red" /></button>
-    </div>
-)
-             
-        })}
-    
- 
-    </div>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
